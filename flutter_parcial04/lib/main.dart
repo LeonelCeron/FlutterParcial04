35bd5686b50d5cdf42b9977b39d5366384040b32
LeonelCeron <ceronleonel99@gmail.com>
2022-11-17 22:05:05 -0600

    The last

--- flutter_parcial04/lib/main.dart
+++ flutter_parcial04/lib/main.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter_parcial04/Clientes.dart';
-import 'package:flutter_parcial04/Principal.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 //import 'package:flutter_parcial04/Pantallas/Principal.dart';
